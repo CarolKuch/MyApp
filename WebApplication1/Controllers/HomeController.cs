8d6eda85c6e5304f99e63f54f447ea19f0282b57
CarolKuch <kar.kucharska@gmail.com>
2024-03-12 11:55:33 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : MyController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
